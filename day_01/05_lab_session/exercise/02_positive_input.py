a93f3dc258a47b1882e0ef1a91c4fb890e160e50
pau25381-stack <pau25381@gmail.com>
2026-06-27 19:25:32 +0800

    day 1

--- day_01/05_lab_session/exercise/02_positive_input.py
+++ day_01/05_lab_session/exercise/02_positive_input.py
@@ -1,8 +1,34 @@
 # TODO: Ask the user for an input that should be a number
-number = input("Enter number: ")
+from enum import nonmember
+
+
+def get_number_input():
+    number = int(input("Enter a number: "))
+
+    try:
+        number_converted = int(number)
+        if number_converted < 0:
+            raise ValueError ()
+
+        return number_converted
+    except ValueError:
+        print("Number is not valid!")
+
+    return None
+
+def get_safe_number_input():
+    number = get_number_input()
+    while not number:
+        number = get_number_input()
+
+    return number
+
+var = get_number_input()
+
+get_safe_number_input()
 
 # TODO: Then try to convert this into an integer using the following:
-number_converted = int(number)
+#number_converted = int(number)
 
 # The user could provide an invalid integer input (string)
 # TODO: Handle this case
